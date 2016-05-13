46080a129fd4318a02df0abf500c0bf4d9dd0933
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2016-05-13 20:22:05 +0530

    Scaffolding the frontend out.
    Run 'yo angular NoveaBank'. Use Grunt for build.
    Do no use Sass with Compass. Use Bootstrap.
    Use the default set of Angular modules.
    Overwrite package.json when an error occurs.

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noveaBankApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the noveaBankApp
+ */
+angular.module('noveaBankApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noveaBankApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the noveaBankApp
+ */
+angular.module('noveaBankApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
